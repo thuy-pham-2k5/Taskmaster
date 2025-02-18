181a6c4a383e8b0c9e12d6b5969645cc292a1c79
thehongbaokhanh <145318138+thehongbaokhanh@users.noreply.github.com>
2025-02-18 11:28:35 +0700

    update the board interface

--- src/main/java/com/example/taskmaster/database/ConnectDatabase.java
+++ src/main/java/com/example/taskmaster/database/ConnectDatabase.java
@@ -11,10 +11,7 @@ public class ConnectDatabase {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String password = System.getenv("dbPassword");
-<<<<<<< HEAD
             System.out.println("Value of password: " + password);
-=======
->>>>>>> 5f2150360dbb9b698523a50e20440b063a0b2105
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/taskmaster", "root", password);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
