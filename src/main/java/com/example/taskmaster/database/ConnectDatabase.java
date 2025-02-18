3026d74ae563bbbf1bbfc179577489dae7d9ca71
Thuy Pham <phamthikimthuy05@gmail.com>
2025-02-18 16:58:20 +0700

    completed sign up

--- src/main/java/com/example/taskmaster/database/ConnectDatabase.java
+++ src/main/java/com/example/taskmaster/database/ConnectDatabase.java
@@ -9,7 +9,7 @@ public class ConnectDatabase {
     public static Connection getConnection() {
         Connection connection;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String password = System.getenv("dbPassword");
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/taskmaster", "root", password);
         } catch (ClassNotFoundException | SQLException e) {
--- src/main/java/com/example/taskmaster/service/authenticate/IAuthenticateService.java
+++ src/main/java/com/example/taskmaster/service/authenticate/IAuthenticateService.java
@@ -3,7 +3,7 @@ package com.example.taskmaster.service.authenticate;
 import com.example.taskmaster.model.User;
 
 public interface IAuthenticateService {
-    void signUp (User user);
+    Boolean signUp (User user);
     void signIn (String email, String password);
     void logout ();
     void resetPassword (String email);
