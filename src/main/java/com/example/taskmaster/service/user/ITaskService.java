9ad3c8d89be58eff2c8f13a611f1ae2fdf7c01c6
Thuy Pham <phamthikimthuy05@gmail.com>
2025-03-14 12:22:44 +0700

    update

--- src/main/java/com/example/taskmaster/service/user/ITaskService.java
+++ src/main/java/com/example/taskmaster/service/user/ITaskService.java
@@ -1,15 +1,19 @@
 package com.example.taskmaster.service.user;
 
+import com.example.taskmaster.model.DetailTask;
 import com.example.taskmaster.model.Task;
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+
 import java.util.List;
 import java.util.Map;
 
 public interface ITaskService {
     List<Integer> getAllColumnId (int boardId);
     Map<Integer, List<Task>> getAllTask (List<Integer> columnIds);
+    Map<Task, List<DetailTask>> getDetailTask (int taskId);
     Task createTask (String title, int listId);
     boolean deleteTask (int taskId);
     boolean deleteAllTaskInColumn (int columnId);
-    
+
 
 }
